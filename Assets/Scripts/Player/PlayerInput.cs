14c5739be4f5a5824964980c1dfbffc2424852d7
Theo.bsl <t.basile@lahorde.school>
2024-01-11 23:24:00 +0100

    camera movement with new unity input system

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,53 +1,17 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private PlayerMovement _playerMovement;
 
-    private bool b_isAzerty = true;
-
-    private void Update()
+    public void OnMove(InputAction.CallbackContext context)
     {
-        MovementInput();
-        ConstructionInput();
+        _playerMovement.SetDirection(context.ReadValue<Vector2>());
     }
 
-    private void MovementInput()
+    public void OnZoom(InputAction.CallbackContext context)
     {
-        if ((b_isAzerty && Input.GetKey(KeyCode.Z)) || (!b_isAzerty && Input.GetKey(KeyCode.W)))
-        {
-            _playerMovement.MoveForward();
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            _playerMovement.MoveBackward();
-        }
-
-        if ((b_isAzerty && Input.GetKey(KeyCode.Q)) || (!b_isAzerty && Input.GetKey(KeyCode.A)))
-        {
-            _playerMovement.MoveLeft();
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            _playerMovement.MoveRight();
-        }
-
-        float scroll = Input.mouseScrollDelta.y;
-
-        if (scroll != 0)
-            _playerMovement.MoveZoom(scroll);
-    }
-
-    private void ConstructionInput()
-    {
-        if (Input.GetMouseButtonUp(0))
-        {
-            
-        }
-        else if (Input.GetMouseButtonUp(1))
-        {
-            
-        }
+        _playerMovement.SetZoom(context.ReadValue<float>());
     }
 }
